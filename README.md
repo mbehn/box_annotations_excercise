758231325ab718179ed6eb1cab884cc87b134f11
Matthew Behn <matthew.behn@gmail.com>
2018-10-02 08:09:58 -0500

    second commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Box View + Annotations Example
+A simple app made with the the Box Node SDK and Box View with Annotations
+
+## Machine Setup
+- Download and install [Sublime Text 3](https://www.sublimetext.com/3) if you don't already have it. Feel free to use your Text Editor of choice.
+- Make a new Box View App (App Token) Token Application in your preferred Box demo account
+- Install [Node.js](https://nodejs.org/en/download/)
+- The installation of Node.js includes NPM which is the package manager for Node applications
+  - Make sure to check Node is installed by running the terminal command: node -v
+  - If a version number is returned, you're good. If you get an error that the command couldn't be found, try installing again
+
+## Box Application Setup
+
+1. Login to your Box Developer console
+2. Create a new App Token Application
+3. Generate a new App Token and save it somewhere
+4. In the CORS domains configuration add <code>localhost:8080</code>
--- app.js
+++ app.js
@@ -1,32 +1,60 @@
+/*
+	- Require in the dependencies
+*/
 const BoxSDK = require('box-node-sdk');
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 
+/*
+	 - Setting the express instance for our server with express().
+*/
 let app = express();
+
+/*
+	- Setting the port for the application
+*/
 let port = process.env.PORT || 8080;
 
-// Set app engine
+/* 
+	 - Set Templating Engine engine of our express instance (app) to mustacheExpress()
+*/
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-// Set Views Directory
-app.set('views', './views')
-// Set static directory
+
+/*
+	- Set the views directory, where our static .mustache files will live.
+*/
+
+app.set('views', './views');
+
+/*
+	- Set the static files directory. This while hold any Javascript (.js) or CSS files (.css)
+*/
 app.use(express.static('static'));
 
+/*
+	- Assign your example file IDs to variables. Feel free to add any other example files here and assign them to a unique variable.s	
+*/
 const wrdFileId = '<YOUR_WORD_FILE_ID>';
 const pptFileID = '<YOUR_PPT_FILE_ID>';
 const xlsFileID = '<YOUR_EXCEL_FILE_ID>';
 
-let scopes = "item_preview annotation_edit";
+/* ###################### Setup the Box SDK ######################*/
 
+
+// Initialize an instance of the Box SDK with the Client ID
 const sdk = new BoxSDK({
 	clientID: '<CLIENT_ID>',
 	clientSecret: ''
 });
 
-const appToken = '<CLIENT_SECRET>';
+// Set the Application Token as a variable
+
+const appToken = '<APP_TOKEN>';
 
 let client = sdk.getBasicClient(appToken);
+
+// Annotator token options
 
 let options = {
 	actor: {
@@ -35,10 +63,22 @@ let options = {
 	}
 };
 
+/*
+	- Setup Views and their logic.
+*/
 app.get('/', (req, res) => {
 	res.render('index');
 });
 
+// Set the scopes for Token Exchange
+
+/*
+	- We will use the exchangeToken() method from the Node SDK to get our Annotator Token
+*/
+
+const scopes = '';
+
+// Word File
 app.get('/boxviewword', (req, res) => {
 client.exchangeToken(scopes, `https://api.box.com/2.0/files/${wrdFileId}`, options)
 	.then(tokenInfo => {
@@ -50,6 +90,8 @@ client.exchangeToken(scopes, `https://api.box.com/2.0/files/${wrdFileId}`, optio
 	})
 });
 
+// PowerPoint file
+
 app.get('/boxviewppt', (req, res) => {
 client.exchangeToken(scopes, `https://api.box.com/2.0/files/${pptFileID}`, options)
 	.then(tokenInfo => {
@@ -60,6 +102,8 @@ client.exchangeToken(scopes, `https://api.box.com/2.0/files/${pptFileID}`, optio
 		})
 	})
 });
+
+// Excel file
 
 app.get('/boxviewxls', (req, res) => {
 client.exchangeToken(scopes, `https://api.box.com/2.0/files/${xlsFileID}`, options)
